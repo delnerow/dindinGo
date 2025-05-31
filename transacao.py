bdaa8d4b4ab86d86004d5d7a145c412ee7a0b70d
Kaneda-prog <59671354+Kaneda-prog@users.noreply.github.com>
2025-05-30 22:43:40 -0300

    metade ansiedade

---
+++ transacao.py
@@ -0,0 +1,14 @@
+
+
+class Carteira():
+    def __init__(self):
+        pass
+
+
+class Transaction():
+    def __init__(self,nome,valor, tipo, data, repeticao):
+        self.nome = nome
+        self.valor = valor 
+        self.tipo =tipo
+        self.data = data
+        self.repeticao = repeticao
